9f87f2ff9949afd46fd038eb3bf6d63363b0da74
Ben Nham <nham@cs.stanford.edu>
2013-02-10 21:41:01 -0800

    Add the file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-pdfcat
-======
+pdfcat.py
+=========
+
+A simple script that concatenates input pdfs to an output pdf using OS X's built-in PDFKit framework. It should work with any recent OS X's Python out of the box.
---
+++ pdfcat.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''Concatenate input pdfs to an output pdf using OS X's built-in PDFKit framework.
+   Should work with any recent OS X\'s system Python out of the box.'''
+
+import optparse
+import os
+import sys
+import objc
+from Quartz import *
+
+__author__ = 'Ben Nham'
+__license__ = 'WTFPL'
+
+parser = optparse.OptionParser(usage='pdfcat [-f] 1.pdf 2.pdf 3.pdf ... output.pdf')
+parser.add_option('-f', '--force', action='store_true', dest='force', help='force write even if output.pdf exists')
+
+options, args = parser.parse_args()
+
+if len(args) < 2:
+	parser.error('Must specify at least one input file and one output file.')
+
+dstpath = args[-1]
+paths = args[:-1]
+
+if os.path.exists(dstpath) and not options.force:
+	parser.error('Bailing since %s already exists. Use -f to force overwrite.' % dstpath)
+
+dstdoc = PDFDocument.alloc().init()
+
+for path in paths:
+	doc = PDFDocument.alloc().initWithURL_(NSURL.fileURLWithPath_(path))
+
+	for i in range(doc.pageCount()):
+		page = doc.pageAtIndex_(i)
+		dstdoc.insertPage_atIndex_(page, dstdoc.pageCount())
+
+dstdoc.writeToFile_(dstpath)
