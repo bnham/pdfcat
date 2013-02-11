05d01e258bb7b7dc0aa27d5b3ea1561caa145b3a
Ben Nham <nham@cs.stanford.edu>
2013-02-11 10:04:17 -0800

    fix indentation, fix force option

--- pdfcat.py
+++ pdfcat.py
@@ -13,26 +13,32 @@ __author__ = 'Ben Nham'
 __license__ = 'WTFPL'
 
 parser = optparse.OptionParser(usage='pdfcat [-f] 1.pdf 2.pdf 3.pdf ... output.pdf')
-parser.add_option('-f', '--force', action='store_true', dest='force', help='force write even if output.pdf exists')
+parser.add_option('-f', '--force', action='store_true', dest='force', help='force write even if output.pdf exists, or if input pdf can\'t be opened')
 
 options, args = parser.parse_args()
 
 if len(args) < 2:
-	parser.error('Must specify at least one input file and one output file.')
+    parser.error('Must specify at least one input file and one output file.')
 
 dstpath = args[-1]
 paths = args[:-1]
 
 if os.path.exists(dstpath) and not options.force:
-	parser.error('Bailing since %s already exists. Use -f to force overwrite.' % dstpath)
+    parser.error('Bailing since %s already exists. Use -f to force overwrite.' % dstpath)
 
 dstdoc = PDFDocument.alloc().init()
 
 for path in paths:
-	doc = PDFDocument.alloc().initWithURL_(NSURL.fileURLWithPath_(path))
+    doc = PDFDocument.alloc().initWithURL_(NSURL.fileURLWithPath_(path))
 
-	for i in range(doc.pageCount()):
-		page = doc.pageAtIndex_(i)
-		dstdoc.insertPage_atIndex_(page, dstdoc.pageCount())
+    if not doc:
+        if not options.force:
+            parser.error('Bailing since %s can\'t be opened. Use -f to force creation anyway.' % path)
+        else:
+            continue
+    
+    for i in range(doc.pageCount()):
+        page = doc.pageAtIndex_(i)
+        dstdoc.insertPage_atIndex_(page, dstdoc.pageCount())
 
 dstdoc.writeToFile_(dstpath)
